e7d9e99a29d73ec1691c9a15c937ad524cbb33b0
Sassa1993 <118011618+MiraiTsukiGames@users.noreply.github.com>
2024-05-29 10:03:49 +0200

    update

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def main():
    text = book_text(book_path)
    words = get_num_words(text)
    lower_case = get_chars_dict(text)
-   print(lower_case)
+   chars_sorted_list = chars_dict_to_sorted_list(lower_case)
+   print(f"--- Begin report of {book_path} ---")
+   print(f"{words} words found in the document")
+   print()
 
+   for item in chars_sorted_list:
+      if not item["char"].isalpha():
+            continue
+      print(f"The '{item['char']}' character was found {item['num']} times")
+
+   print("--- End report ---")
 def get_chars_dict(text):
     chars = {}
     for c in text:
@@ -23,7 +32,15 @@ def book_text(path):
      with open(path) as file:
         return file.read()
 
-       
+def sort_on(dict):
+    return dict["num"]
 
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
 
 main()
